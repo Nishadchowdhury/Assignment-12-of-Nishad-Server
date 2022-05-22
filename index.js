2deb8a5f2dd6aca6e13bb26712ee6038d3b8ac48
Nishadchowdhury <Nishadhj111@gmail.com>
2022-05-22 14:08:37 +0600

    root api setup , listen add

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+//root of the server
+app.get('/', async (req, res) => {
+    res.send('server is running Laparts in port ' + port)
+})
+
+app.listen(port, () => {
+    console.log(`running the server in port ${port}`);
+})

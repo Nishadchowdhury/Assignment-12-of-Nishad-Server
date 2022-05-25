625a4a61c95e18af9af922f95ffd927a8567c1e0
Nishadchowdhury <Nishadhj111@gmail.com>
2022-05-25 22:05:26 +0600

    add api for payment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const query = require('express/lib/middleware/query');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -95,6 +96,20 @@ async function runServer() {
 
 
 
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const product = req.body;
+            const price = product.price;
+            const amount = price * 100;
+
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
 
     } finally {
 
